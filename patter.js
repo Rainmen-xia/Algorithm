aeed0ff4f05bd589fa6fd9c70d3ec2cbebf9d489
Rainmen1988 <250809669@qq.com>
2015-06-29 01:13:32 +0800

    ok

---
+++ patter.js
@@ -0,0 +1,14 @@
+;"use strict";
+function partern(n){
+var output = '';
+for(var i =1;i<2*n;i++){
+  var j=0;
+  while(j<2*n){
+    output +=(i>n)?((i==j||(i==2*n-j))?(2*n-i)%10:' '):((i==j||(i==2*n-j))?i%10:' ');
+    j++;
+  }
+output +='\n';
+}
+return output
+}
+console.log(partern(3));
